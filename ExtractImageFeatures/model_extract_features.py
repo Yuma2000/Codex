0c67952b754f2ab2f3cfee630cea307f344dee6c
Yuma2000 <t.yuma2618@gmail.com>
2022-05-08 15:25:29 +0900

    convnextのクラスを使えるように変更

--- ExtractImageFeatures/model_extract_features.py
+++ ExtractImageFeatures/model_extract_features.py
@@ -2,23 +2,16 @@
 特徴抽出に用いるモデルを定義
 author: Kouki Nishimoto, Yuma Takeda
 """
-import os
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torchvision.models as models
-
-# --- Import section for ConvNeXt --- #
 from transformers import ConvNextFeatureExtractor, ConvNextModel
-from datasets import load_dataset
 
-# --- Variable declaration section --- #
 resnet50_model_path = "/home/kouki/Models/resnet50-19c8e357.pth"
 
-# --- ResNet50 Model section --- #
-class I2FEncoder(nn.Module):
+class ResNet50Encoder(nn.Module):
     def __init__(self):
-        super(I2FEncoder, self).__init__()
+        super(ResNet50Encoder, self).__init__()
         self.resnet50 = models.resnet50()
         self.resnet50.load_state_dict(torch.load(resnet50_model_path))
         del self.resnet50.fc
@@ -37,17 +30,15 @@ class I2FEncoder(nn.Module):
         x = x.view(x.size(0), -1)
         return x
 
-# --- ConvNeXt Model section --- #
+
 class ConvNextEncoder():
-    def __init__(self):
-        dataset = load_dataset("huggingface/cats-image")
-        image = dataset["test"]["image"][0]
+  def __init__(self):
+    self.feature_extractor = ConvNextFeatureExtractor.from_pretrained("facebook/convnext-xlarge-224-22k-1k")
+    self.model = ConvNextModel.from_pretrained("facebook/convnext-xlarge-224-22k-1k")
 
-        feature_extractor = ConvNextFeatureExtractor.from_pretrained("facebook/convnext-xlarge-224-22k-1k")
-        model = ConvNextModel.from_pretrained("facebook/convnext-xlarge-224-22k-1k")
-        inputs = feature_extractor(image, return_tensors="pt")
-
-        with torch.no_grad():
-            outputs = model(**inputs)
-
-        last_hidden_states = outputs.last_hidden_state
+  # 引数imgを2048次元の特徴量にして返す．
+  def encode(self, img):
+    inputs = self.feature_extractor(img, return_tensors="pt")
+    with torch.no_grad():
+      outputs = self.model(**inputs)
+    return outputs.last_hidden_state
